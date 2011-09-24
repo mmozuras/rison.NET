5de9b44beee97d6b1a005dd4ea9545d92e0cb2c8
mmozuras <mindaugas.mozuras@gmail.com>
2011-09-24 17:13:54 +0300

    Created solution structure

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("rison.NET")]
+[assembly: AssemblyDescription("Rison encoder and decoder for .NET")]
+[assembly: AssemblyCompany("rison.NET")]
+[assembly: AssemblyProduct("rison.NET")]
+[assembly: AssemblyCopyright("Copyright (C) Mindaugas Mozūras")]
+[assembly: AssemblyVersion("1.0.0")]
+
